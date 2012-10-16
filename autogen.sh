50f0c8f03ade84d548fda06c27cdc293d727d4be
Perberos <perberos@gmail.com>
2012-10-16 17:27:11 -0300

    fixing some deprecated variable name on configure file

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="mate-desktop"
 
-(test -f $srcdir/configure.in) || {
+(test -f $srcdir/configure.ac) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
