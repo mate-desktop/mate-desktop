47426c90d10e9f738ecf89f35db94ca8deff55e0
Jasmine Hassan <jasmine.aura@gmail.com>
2012-12-17 11:00:57 +0200

    [mate-bg] small cleanup

--- libmate-desktop/mate-bg.c
+++ libmate-desktop/mate-bg.c
@@ -2020,18 +2020,19 @@ static gboolean
 blow_expensive_caches (gpointer data)
 {
 	MateBG *bg = data;
-	GList *list, *next;
+	GList *list;
 
 	bg->blow_caches_id = 0;
 
-	for (list = bg->file_cache; list != NULL; list = next) {
-		FileCacheEntry *ent = list->data;
-		next = list->next;
+	if (bg->file_cache) {
+		for (list = bg->file_cache; list != NULL; list = list->next) {
+			FileCacheEntry *ent = list->data;
 
-		if (ent->type == PIXBUF) {
-			file_cache_entry_delete (ent);
-			bg->file_cache = g_list_delete_link (bg->file_cache,
-							     list);
+			if (ent->type == PIXBUF) {
+				file_cache_entry_delete (ent);
+				bg->file_cache = g_list_delete_link (bg->file_cache,
+								     list);
+			}
 		}
 	}
 
