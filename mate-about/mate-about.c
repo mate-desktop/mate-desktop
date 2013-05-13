9f380946feadd7e5bb8247d9de2ae6e181471d56
Stefano Karapetsas <stefano@karapetsas.com>
2013-05-13 13:19:38 +0200

    mate-about: Fix g_type_init deprecation in glib 2.36

--- mate-about/mate-about.c
+++ mate-about/mate-about.c
@@ -140,7 +140,9 @@
 
 		mate_gettext(GETTEXT_PACKAGE, LOCALE_DIR, "UTF-8");
 
+		#if !GLIB_CHECK_VERSION (2, 36, 0)
 		g_type_init();
+		#endif
 
 		/* http://www.gtk.org/api/2.6/glib/glib-Commandline-option-parser.html */
 		GOptionContext* context = g_option_context_new(NULL);
