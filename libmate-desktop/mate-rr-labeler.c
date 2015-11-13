mate-desktop/mate-desktop

commit 11757da17e9fcdd33474aaa342bf4b6a2daf82d0

mate-rr-labeler: Show output name in addition to display name.

libmate-desktop/mate-rr-labeler.c
@@ -420,7 +420,7 @@ create_label_window (MateRRLabeler *labeler, MateRROutputInfo *output, GdkColor 
 	GtkWidget *window;
 	GtkWidget *widget;
 	char *str;
-	const char *display_name;
+	char *display_name;
 	GdkColor black = { 0, 0, 0, 0 };
 	int x,y;
 
@@ -452,10 +452,13 @@ create_label_window (MateRRLabeler *labeler, MateRROutputInfo *output, GdkColor 
 		 * translation could be "Pantallas en Espejo", *not* "Espejar Pantallas".
 		 */
 		display_name = _("Mirror Screens");
-	} else
-		display_name = mate_rr_output_info_get_display_name (output);
+		str = g_strdup_printf ("<b>%s</b>", display_name);
+	} else {
+		display_name = g_strdup_printf ("<b>%s</b>\n<small>%s</small>", mate_rr_output_info_get_display_name (output), mate_rr_output_info_get_name (output));
+		str = g_strdup_printf ("%s", display_name);
+	}
+	g_free (display_name);
 
-	str = g_strdup_printf ("<b>%s</b>", display_name);
 	widget = gtk_label_new (NULL);
 	gtk_label_set_markup (GTK_LABEL (widget), str);
 	g_free (str);

commit 06ccb7a39e494e731436f7e6de044ff113c14896

display properties: don't try to free a string literal (fix crash)
crash introduced in https://github.com/mate-desktop/mate-desktop/commit/11757da17e9fcdd33474aaa342bf4b6a2daf82d0

libmate-desktop/mate-rr-labeler.c
@@ -451,7 +451,7 @@ create_label_window (MateRRLabeler *labeler, MateRROutputInfo *output, GdkColor 
 		 * used as an adjective, not as a verb.  For example, the Spanish
 		 * translation could be "Pantallas en Espejo", *not* "Espejar Pantallas".
 		 */
-		display_name = _("Mirror Screens");
+		display_name = g_strdup_printf (_("Mirror Screens"));
 		str = g_strdup_printf ("<b>%s</b>", display_name);
 	} else {
 		display_name = g_strdup_printf ("<b>%s</b>\n<small>%s</small>", mate_rr_output_info_get_display_name (output), mate_rr_output_info_get_name (output));