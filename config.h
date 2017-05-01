44241856246c0064b893259a034afc2d2b3c00c0
nokangaroo <nokangaroo@aon.at>
2017-05-01 22:37:59 +0200

    Fix the hardcoded dropper cursor in colorsel.c

---
+++ config.h
@@ -0,0 +1,118 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* always defined to indicate that i18n is enabled */
+#define ENABLE_NLS 1
+
+/* The gettext translation domain */
+#define GETTEXT_PACKAGE "mate-desktop"
+
+/* Warn on use of APIs added after GLib 2.36 */
+#define GLIB_VERSION_MAX_ALLOWED GLIB_VERSION_2_36
+
+/* Warn on use of APIs deprecated before GLib 2.36 */
+#define GLIB_VERSION_MIN_REQUIRED GLIB_VERSION_2_36
+
+/* Define to 1 if you have the `bind_textdomain_codeset' function. */
+#define HAVE_BIND_TEXTDOMAIN_CODESET 1
+
+/* Define to 1 if you have the Mac OS X function CFLocaleCopyCurrent in the
+   CoreFoundation framework. */
+/* #undef HAVE_CFLOCALECOPYCURRENT */
+
+/* Define to 1 if you have the Mac OS X function CFPreferencesCopyAppValue in
+   the CoreFoundation framework. */
+/* #undef HAVE_CFPREFERENCESCOPYAPPVALUE */
+
+/* Define to 1 if you have the `dcgettext' function. */
+#define HAVE_DCGETTEXT 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define if the GNU gettext() function is already present or preinstalled. */
+#define HAVE_GETTEXT 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if your <locale.h> file defines LC_MESSAGES. */
+#define HAVE_LC_MESSAGES 1
+
+/* Define to 1 if you have the <locale.h> header file. */
+#define HAVE_LOCALE_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define if the xrandr-$XRANDR_REQUIRED library is present */
+#define HAVE_RANDR 1
+
+/* Building with startup notification support */
+#define HAVE_STARTUP_NOTIFICATION /**/
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Enable additional debugging at the expense of performance and size */
+/* #undef MATE_ENABLE_DEBUG */
+
+/* Define to the major version */
+#define MATE_MAJOR 1
+
+/* Define to the micro version */
+#define MATE_MICRO 0
+
+/* Define to the minor version */
+#define MATE_MINOR 19
+
+/* Name of package */
+#define PACKAGE "mate-desktop"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "https://github.com/mate-desktop/mate-desktop/"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "mate-desktop"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "mate-desktop 1.19.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "mate-desktop"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.19.0"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "1.19.0"
+
+/* Define to 1 if the X Window System is missing or not being used. */
+/* #undef X_DISPLAY_MISSING */
